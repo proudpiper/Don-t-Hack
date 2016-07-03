28406e339dd65d366f63fe9cfd723d3f3bb11f47
heyacfox <heyacfox@gmail.com>
2016-07-03 10:44:43 -0400

    Added all of the animal sounds

--- Assets/Obstacles/AnimalObstacle.cs
+++ Assets/Obstacles/AnimalObstacle.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class AnimalObstacle : Obstacle{
 
+	public List<AudioClip> startSounds;
 
 	public override void HandleCollision(Player player){
 		player.breath = player.breath - breathDamage;
@@ -10,6 +12,8 @@ public class AnimalObstacle : Obstacle{
 	}
 
 	void Awake() {
-
+		AudioSource audios = this.GetComponent<AudioSource> ();
+		audios.clip = startSounds [Random.Range (0, startSounds.Count)];
+		audios.Play();
 	}
 }
